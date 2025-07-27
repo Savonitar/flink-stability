e17691440430dd53606d1a8f9b485f3764f1962a
Aleksandr Savonin <savonitar@gmail.com>
2025-07-28 00:05:05 +0200

    Add Kafka validations to check that Flink job processed data correctly

--- testcontainers/src/main/java/org/savonitar/testcontainers/ClusterManager.java
+++ testcontainers/src/main/java/org/savonitar/testcontainers/ClusterManager.java
@@ -2,79 +2,211 @@ package org.savonitar.testcontainers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.Container;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class ClusterManager {
     private static final Logger LOG = LoggerFactory.getLogger(ClusterManager.class);
     private final Network network = Network.newNetwork();
+    private KafkaValidator kafkaValidator;
     private ConfluentKafkaContainer kafka;
     private GenericContainer<?> jobManager;
     private GenericContainer<?> taskManager;
+    private String jobManagerRestUrl;
+    private static final int MESSAGE_COUNT = 1_000;
+    private static final String KAFKA_BOOTSTRAP_SERVER = "localhost:9093";
+    private static final String RECORDS_FILE_PATH = "/tmp/records.txt";
+    private static final int CONSUMER_TIMEOUT_MS = 10000;
 
-    public void startKafka() {
+    private static final class KafkaTopicConfig {
+        private final String name;
+        private final int partitions;
+        private final int replicationFactor;
+        private final String bootstrapServer;
+
+        public KafkaTopicConfig(String name, int partitions, int replicationFactor, String bootstrapServer) {
+            this.name = name;
+            this.partitions = partitions;
+            this.replicationFactor = replicationFactor;
+            this.bootstrapServer = bootstrapServer;
+        }
+    }
+
+    public void startKafka() throws IOException, InterruptedException {
         LOG.info("Start Kafka");
-        kafka = new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.0"))
+
+        kafka = new ConfluentKafkaContainer(
+                DockerImageName.parse("confluentinc/cp-kafka:7.4.0")
+                        .asCompatibleSubstituteFor("apache/kafka"))
                 .withNetwork(network)
                 .withNetworkAliases("kafka")
-                .withEnv("KAFKA_PROCESS_ROLES", "broker,controller")
-                .withEnv("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@kafka:9093")
-                .withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092,BROKER://0.0.0.0:9094,CONTROLLER://0.0.0.0:9093")
-                .withEnv("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://localhost:9092,BROKER://kafka:9094")
-                .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER")
-                .withEnv("KAFKA_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
-                .withEnv("KAFKA_LOG_DIRS", "/tmp/kraft-combined-logs")
-                .waitingFor(Wait.forLogMessage(".*Kafka Server started.*", 1))
+                .withListener("kafka:9095")
+                .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "true")
+                .withEnv("KAFKA_TRANSACTION_MAX_TIMEOUT_MS", String.valueOf(Duration.ofHours(2).toMillis()))
+                .withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
+                .withEnv("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1")
+                .withEnv("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
+                .withEnv("KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS", "0")
                 .withStartupTimeout(Duration.ofSeconds(120))
                 .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("KAFKA_CONTAINER_LOGS")));
 
         kafka.start();
+        kafkaValidator = new KafkaValidator(kafka);
         LOG.info("Kafka started at: {}", kafka.getBootstrapServers());
+        createAndFillInInputTopic();
     }
 
-    public void startFlink(String version) {
-        DockerImageName flinkImage = DockerImageName.parse("flink:" + version);
+    private void createAndFillInInputTopic() throws IOException, InterruptedException {
+        KafkaTopicConfig topicConfig = new KafkaTopicConfig(
+                "input-topic",
+                1,
+                1,
+                KAFKA_BOOTSTRAP_SERVER
+        );
 
-        jobManager = new GenericContainer<>(flinkImage)
-                .withNetwork(network)
-                .withExposedPorts(8081)
-                .withEnv("JOB_MANAGER_RPC_ADDRESS", "jobmanager")
-                .withEnv("FLINK_PROPERTIES", "taskmanager.numberOfTaskSlots: 2")
-                .withCommand("jobmanager");
+        createTopic(topicConfig);
+        String messages = generateMessages(MESSAGE_COUNT);
+        produceMessages(topicConfig, messages);
+        consumeAndVerifyMessages(topicConfig);
+        listTopics();
+    }
 
-        taskManager = new GenericContainer<>(flinkImage)
-                .withNetwork(network)
-                .withEnv("JOB_MANAGER_RPC_ADDRESS", "jobmanager")
-                .withEnv("FLINK_PROPERTIES", "taskmanager.numberOfTaskSlots: 2")
-                .withCommand("taskmanager");
+    private void createTopic(KafkaTopicConfig config) throws IOException, InterruptedException {
+        LOG.info("Creating topic '{}'", config.name);
+        kafka.execInContainer(
+                "/bin/sh", "-c",
+                "kafka-topics",
+                "--create",
+                "--if-not-exists",
+                "--topic", config.name,
+                "--bootstrap-server", config.bootstrapServer,
+                "--partitions", String.valueOf(config.partitions),
+                "--replication-factor", String.valueOf(config.replicationFactor)
+        );
+    }
 
+    private String generateMessages(int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= count; i++) {
+            sb.append(i).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private void produceMessages(KafkaTopicConfig config, String messages) throws IOException, InterruptedException {
+        LOG.info("Producing {} messages into '{}'", MESSAGE_COUNT, config.name);
+        kafka.copyFileToContainer(Transferable.of(messages.getBytes()), RECORDS_FILE_PATH);
+
+        Container.ExecResult result = kafka.execInContainer(
+                "/bin/sh", "-c",
+                String.format("cat %s | kafka-console-producer --broker-list %s --topic %s",
+                        RECORDS_FILE_PATH, config.bootstrapServer, config.name)
+        );
+
+        if (result.getExitCode() != 0) {
+            LOG.error("Failed to produce records. ExitCode={}, Stderr={}",
+                    result.getExitCode(), result.getStderr());
+        } else {
+            LOG.info("Successfully produced messages to '{}'", config.name);
+        }
+    }
+
+    private void consumeAndVerifyMessages(KafkaTopicConfig config) throws IOException, InterruptedException {
+        Container.ExecResult result = kafka.execInContainer(
+                "/bin/sh", "-c",
+                String.format("kafka-console-consumer --bootstrap-server %s --topic %s " +
+                                "--from-beginning --timeout-ms %d --max-messages %d " +
+                                "--consumer-property group.id=test-input-group " +
+                                "--consumer-property isolation.level=read_uncommitted",
+                        config.bootstrapServer, config.name, CONSUMER_TIMEOUT_MS, MESSAGE_COUNT)
+        );
+        LOG.info("InputConsumer output: {}", result.getStdout());
+    }
+
+    private void listTopics() throws IOException, InterruptedException {
+        Thread.sleep(5000);
+        Container.ExecResult execResult = kafka.execInContainer(
+                "/bin/sh", "-c",
+                "kafka-topics", "--list",
+                "--bootstrap-server localhost:9093");
+        LOG.info("Topics output: {}", execResult.getStdout());
+    }
+
+    public void startFlink(String version) throws InterruptedException, IOException {
+        registerShutdownHook();
+
+        FlinkContainer containerConfig = new FlinkContainer(version, network);
+        jobManager = containerConfig.createJobManager();
+        taskManager = containerConfig.createTaskManager();
+
+        startContainers();
+        configureJobManagerUrl();
+        logFlinkStartup();
+    }
+
+    private void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                if (taskManager != null) taskManager.stop();
+                if (jobManager != null) jobManager.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+
+    private void startContainers() {
         jobManager.start();
         taskManager.start();
+    }
 
-        LOG.info("Flink JobManager started at: http://localhost:{}", jobManager.getMappedPort(8081));
+    private void configureJobManagerUrl() {
+        int restPort = jobManager.getMappedPort(FlinkContainer.JOB_MANAGER_PORT);
+        jobManagerRestUrl = "http://localhost:" + restPort;
+    }
+
+    private void logFlinkStartup() {
+        String flinkWebUi = jobManagerRestUrl;
+        LOG.info("Flink JobManager started at: {}", jobManagerRestUrl);
+        LOG.info("Flink Web UI: {}", flinkWebUi);
+    }
+
+    public boolean waitForAndValidateKafkaOutput(String topic, int expectedMessages) throws Exception {
+        return kafkaValidator.waitForAndValidateKafkaOutput(topic, expectedMessages);
+    }
+
+    public boolean performFinalValidation(String topic, int expectedMessages) throws Exception {
+        return kafkaValidator.performFinalValidation(topic, expectedMessages);
+    }
+
+    public void printKafka() throws InterruptedException, IOException {
+        kafkaValidator.printMessages("flink-output", CONSUMER_TIMEOUT_MS, 1000);
     }
 
     public void stopFlink() {
-        if (taskManager != null) taskManager.stop();
-        if (jobManager != null) jobManager.stop();
-    }
-
-
-    public void stopAll() {
         if (taskManager != null) {
             taskManager.stop();
         }
         if (jobManager != null) {
             jobManager.stop();
         }
-        if (kafka != null) {
-            kafka.stop();
-        }
+    }
+
+    public void stopAll() {
+        LOG.info("ClusterManager stopping everything.");
+        stopFlink();
+        if (kafka != null) kafka.stop();
+    }
+
+    public String getJobManagerRestUrl() {
+        return jobManagerRestUrl;
     }
 }
---
+++ testcontainers/src/main/java/org/savonitar/testcontainers/FlinkContainer.java
@@ -0,0 +1,50 @@
+package org.savonitar.testcontainers;
+
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.nio.file.Paths;
+
+public class FlinkContainer {
+    private static final String CHECKPOINT_PATH = "/flink/checkpoints";
+    private static final String LOCAL_CHECKPOINT_PATH = Paths.get("checkpoints").toAbsolutePath().toString();
+    private static final String TASK_SLOTS_PROPERTY = "taskmanager.numberOfTaskSlots: 2";
+    private static final String JOB_MANAGER_ALIAS = "jobmanager";
+    public static final int JOB_MANAGER_PORT = 8081;
+
+    private final DockerImageName flinkImage;
+    private final Network network;
+
+    public FlinkContainer(String version, Network network) {
+        this.flinkImage = DockerImageName.parse("flink:" + version);
+        this.network = network;
+    }
+
+    public GenericContainer<?> createJobManager() {
+        return new GenericContainer<>(flinkImage)
+                .withNetwork(network)
+                .withNetworkAliases(JOB_MANAGER_ALIAS)
+                .withExposedPorts(JOB_MANAGER_PORT)
+                .withFileSystemBind(LOCAL_CHECKPOINT_PATH, CHECKPOINT_PATH)
+                .withEnv("JOB_MANAGER_RPC_ADDRESS", JOB_MANAGER_ALIAS)
+                .withEnv("FLINK_PROPERTIES", TASK_SLOTS_PROPERTY)
+                .withCommand("jobmanager")
+                .withLogConsumer(createLogConsumer("JOB_MANAGER_LOGS"));
+    }
+
+    public GenericContainer<?> createTaskManager() {
+        return new GenericContainer<>(flinkImage)
+                .withNetwork(network)
+                .withEnv("JOB_MANAGER_RPC_ADDRESS", JOB_MANAGER_ALIAS)
+                .withEnv("FLINK_PROPERTIES", TASK_SLOTS_PROPERTY)
+                .withCommand("taskmanager")
+                .withLogConsumer(createLogConsumer("TASK_MANAGER_LOGS"));
+    }
+
+    private Slf4jLogConsumer createLogConsumer(String loggerName) {
+        return new Slf4jLogConsumer(LoggerFactory.getLogger(loggerName));
+    }
+}
